a0f846ca682a6996794fc4c2e383a11ca1cc4142
Dhavalkumar Gajjar <dhaval2g@gmail.com>
2019-02-21 13:13:57 -0500

    EDITED

--- fun.js
+++ fun.js
@@ -1,24 +1,46 @@
 function showMe () {
-    var yname = document.getElementById("val-nm").value;
-    var yem = document.getElementById("val-email").value;
-    var ypw = document.getElementById("val-pw").value;
-    var ycpw = document.getElementById("val-cpw").value;
+  var yname = document.getElementById('val-nm').value
+  var yem = document.getElementById('val-email').value
+  var ypw = document.getElementById('val-pw').value
+  var ycpw = document.getElementById('val-cpw').value
 
-    document.getElementById("rnm").value = yname;
-    document.getElementById("rem").value = yem;
-    document.getElementById("rpw").value = ypw;
-    document.getElementById("rcpw").value = ycpw;
-    // if (yem == -1) {
-    //     alert("Not a valid e-mail!");
-    //     submitOK = "false";
-    //   }
-    }
+  document.getElementById('rnm').value = yname
+  document.getElementById('rem').value = yem
+  document.getElementById('rpw').value = ypw
+  document.getElementById('rcpw').value = ycpw
+}
+function hidD () {
+  var x = document.getElementById('result')
+  if (x.style.display === 'none') {
+    x.style.display = 'block'
+  }
+  else {
+    x.style.display = 'none'
+  }
+}
+function validme(){
+  var nm = document.forms["vfrm"]["username"].value;
+  if (nm == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+  var nm = document.forms["vfrm"]["val-email"].value;
+  if (nm == "") {
+    alert("Email must be filled out");
+    return false;
+  }
+  var pw = document.forms["vfrm"]["userpw"].value;
+  var ypw = document.getElementById('val-pw').value;
+  if (pw == ""|| ypw.length < 6) {
+    alert("Password must be filled out");
+    return false;
+  }
 
-    function hidD() {
-        var x = document.getElementById("result");
-        if (x.style.display === "none") {
-          x.style.display = "block";
-        } else {
-          x.style.display = "none";
-        }
-      }
+  var password = document.getElementById("val-pw")
+  , confirm_password = document.getElementById("val-cpw");
+
+  if(password.value != confirm_password.value) {
+    alert("Passwords Don't Match");
+    return false;
+  }
+}
